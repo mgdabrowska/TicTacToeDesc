870d08b96bbbb7430468cbb8b5af8db144cf1e58
Stawicka Malgorzata (UniCredit Business Integrated Solutions) <C311681@hd00.UNICREDITGROUP.EU>
2017-08-23 09:21:17 +0200

    First commit

---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/application/TicTacToe.fxml"));
+		//	Parent root = FXMLLoader.load(getClass().getResource("/application/TicTacToe.fxml"));
+			//BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,600,600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			 primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
